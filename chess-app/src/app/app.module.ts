4e9f921b42b93c330493f24bccefd7b2302cfb0c
Colin Barnes <barnesc775@gmail.com>
2019-05-17 10:05:02 -0400

    added initial pieces component and routing

--- chess-app/src/app/app.module.ts
+++ chess-app/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PiecesComponent } from './pieces/pieces.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PiecesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
