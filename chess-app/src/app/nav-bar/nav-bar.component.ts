e094b09910b48a66fab417c6ae0c848df4288ce7
Colin Barnes <barnesc775@gmail.com>
2019-05-19 16:43:31 -0400

    add function to close nav bar on outside click

--- chess-app/src/app/nav-bar/nav-bar.component.ts
+++ chess-app/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -9,12 +9,23 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+  isOpen: boolean;
+  @ViewChild('drawer') sidenav: any;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private eRef: ElementRef) {}
 
+  // Close the side nav if you click outside of the component
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    if (!this.eRef.nativeElement.contains(event.target)) {  // if click is outside of component
+      if (this.sidenav.opened) {
+        this.sidenav.opened = false;
+      }
+    }
+  }
 }
